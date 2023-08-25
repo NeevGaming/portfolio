f51cee798debe86e22b13d2e1d07c759001adc22
NeevGaming <107578072+NeevGaming@users.noreply.github.com>
2023-08-25 20:11:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ document.getElementById("menu").onclick = function myfunc(){
     if (document.getElementById("menu").innerHTML == "menu"){
         document.getElementById("menu").innerHTML = "close"
         document.getElementById("menu").classList.add("active")
+        document.getElementById("menu_o").style.display = "block";
     }
     else{
         document.getElementById("menu").innerHTML = "menu"
+        document.getElementById("menu_o").style.display = "none";
         document.getElementById("menu").classList.remove("active")
     }
+    
 }
 document.getElementById("home").onclick = function home(){
     location.replace("index.html")
@@ -14,7 +17,16 @@ document.getElementById("home").onclick = function home(){
 document.getElementById("search").onclick = function search(){
     location.replace("search.html")
 }
-document.getElementById("settings").onclick = function search(){
+document.getElementById("settings").onclick = function settings(){
+    location.replace("settings.html")
+}
+document.getElementById("home_android").onclick = function home_android(){
+    location.replace("index.html")
+}
+document.getElementById("search_android").onclick = function search_anhome_android(){
+    location.replace("search.html")
+}
+document.getElementById("settings_android").onclick = function settings_anhome_android(){
     location.replace("settings.html")
 }
 document.getElementById('fscreen').onclick = function fscreen(){
